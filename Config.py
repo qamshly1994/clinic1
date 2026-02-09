e2a7b3814c5b325283f37f658d5fb2dadd258743
qamshly1994 <abdulrazaqalali2@gmail.com>
2026-02-09 03:02:40 +0300

    Create Config.py

---
+++ Config.py
@@ -0,0 +1,8 @@
+import os
+
+class Config:
+    SECRET_KEY = os.environ.get("SECRET_KEY", "dev-secret")
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL", "sqlite:///clinic.db")
+    if SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
+        SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://", 1)
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
